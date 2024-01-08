39c0b762b0e432318443d1b2fc0f0d97d76d644a
YenaLey <yena.e121@gmail.com>
2024-01-08 20:27:49 +0900

    1월 8일 커밋

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 /*eslint-disable*/
 
 import './App.css';
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import Object3D from './components/Object3D'
 
 function App() {
   return (
     <div>
+
       <nav className="navbar">
         <div className="navbar__logo">
           <a href="#"><img className="navbar__logo--img" src="/logo_main.png"></img></a>
@@ -19,57 +19,18 @@ function App() {
         </ul>
       </nav>
 
-      <Object3D className="main-container"></Object3D>
+      <div className = "main-container">
+        <Object3D></Object3D>
+        <div className="main-container--message">
+          <p style = {{fontSize: 30, fontWeight: 600}}>서울시립대학교 컴퓨터 학술 동아리</p>
+          <p style = {{fontSize: 60, fontWeight: 800}}>QUIPU</p>
+        </div>
+      </div>
 
     </div>
   );
 }
 
-function Object3D() {
 
-  const scene = new THREE.Scene();
-  
-  window.innerHeight = window.innerHeight - 80; //네브바 height 제외
-
-  const camera = new THREE.PerspectiveCamera(47, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0,0,5);
-
-  const renderer = new THREE.WebGLRenderer({
-    alpha : true,
-    antialias : true
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight); 
-
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.minDistance = 2;
-  controls.maxDistance = 5;
-  controls.maxPolarAngle = Math.PI / 2;
-
-  const pointLight = new THREE.PointLight(0xffffff, 100);
-  pointLight.position.set(1,1,1);
-  scene.add(pointLight);
-
-  scene.add(new THREE.PointLightHelper(pointLight, 0.3, 'black'));
-
-  const sphere1 = new THREE.Mesh( new THREE.SphereGeometry( 0.5, 32, 16 ), new THREE.MeshStandardMaterial( { color: "blue" } ) );
-  scene.add( sphere1 );
-
-  function animate(){
-    requestAnimationFrame(animate);
-
-    controls.update();
-    renderer.render(scene, camera);
-  }
-  animate();
-
-  function onWindowResize(){
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-  window.addEventListener('resize', onWindowResize);
-
-  return <div ref={(ref) => ref && ref.appendChild(renderer.domElement)} />;
-}
 
 export default App;
---
+++ src/components/Object3D.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+function Object3D() {
+
+    const scene = new THREE.Scene();
+    
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0,0.5,5);
+  
+    const renderer = new THREE.WebGLRenderer({
+      alpha : true,
+      antialias : true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight); 
+  
+    //
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 2;
+    controls.maxDistance = 5;
+    controls.maxPolarAngle = Math.PI / 2;
+    
+    // light
+
+    const pointLight1 = new THREE.PointLight(0xffffff, 90);
+    pointLight1.position.set(-5,0,5);
+    scene.add(pointLight1);
+
+    const pointLight2 = new THREE.PointLight(0xffffff, 160);
+    pointLight2.position.set(0,10,0);
+    scene.add(pointLight2);
+
+    const ambientLight = new THREE.AmbientLight( 0xffffff, 1.2 );
+    scene.add(ambientLight);
+
+    // mesh
+
+    const sphere_left = new THREE.Mesh( new THREE.SphereGeometry( 0.7, 32, 32 ), new THREE.MeshPhysicalMaterial( { color: "#9AB9FF", roughness: 0.5} ) );
+    scene.add( sphere_left );
+    sphere_left.position.set(-2,0.5,-3);
+
+    const sphere_middle = new THREE.Mesh( new THREE.SphereGeometry( 0.8, 32, 32 ), new THREE.MeshPhysicalMaterial( { color: "#FFD4DF", roughness: 0.4} ) );
+    scene.add( sphere_middle );
+    sphere_middle.position.set(-0.3,-1,-1);
+
+    const sphere_right = new THREE.Mesh( new THREE.SphereGeometry( 0.8, 32, 32 ), new THREE.MeshPhysicalMaterial( { color: "#95CFFE", roughness: 0.4} ) );
+    scene.add( sphere_right );
+    sphere_right.position.set(2.4,-1.4, -1);
+
+    const cone = new THREE.Mesh( new THREE.ConeGeometry( 0.45, 0.8, 32 ) , new THREE.MeshPhysicalMaterial( { color: "#FFD2FF", roughness: 0.5} ));
+    scene.add(cone);
+    cone.position.set(0.6,0.2,-1);
+    cone.rotation.z = -Math.PI / 6;
+    cone.rotation.x = -Math.PI / 10;
+
+    const capsule = new THREE.Mesh( new THREE.CapsuleGeometry( 0.17, 0.3, 32, 32 ), new THREE.MeshPhysicalMaterial( {color: "#FFF8E1", roughness: 0.5} ) );
+    scene.add( capsule );
+    capsule.position.set(2.1,0.8,-1);
+    capsule.rotation.x = Math.PI / 5;
+    capsule.rotation.z = Math.PI / 4;
+
+    const cube = new THREE.Mesh( new THREE.BoxGeometry( 0.3, 0.3, 0.3 ), new THREE.MeshPhysicalMaterial( {color: '#E0EBFF', roughness: 0.5} ) ); 
+    scene.add( cube );
+    cube.position.set(2.8,-0.3,0);
+    cube.rotation.x = -Math.PI / 18;
+    cube.rotation.y = Math.PI / 6;
+    cube.rotation.z = Math.PI / 6;
+  
+    //
+    function animate(){
+      requestAnimationFrame(animate);
+  
+      controls.update();
+      renderer.render(scene, camera);
+    }
+    animate();
+  
+    function onWindowResize(){
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    window.addEventListener('resize', onWindowResize);
+  
+    return <div ref={(ref) => ref && ref.appendChild(renderer.domElement)} />;
+}
+
+export default Object3D;
