b3c78eadc816d99ee7914ac053e21b018be10853
YenaLey <yena.e121@gmail.com>
2024-01-10 22:50:40 +0900

    1월 10일 커밋1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 /*eslint-disable*/
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Object3D from './components/Object3D'
+import Object3D1 from './components/Object3D1'
+import Object3D2 from './components/Object3D2'
 
 function App() {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 480);
+
+  //가로 480px임을 감지해서 다른 컴포넌트는 렌더링하기 위한 함수
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 480);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div>
 
@@ -20,17 +34,17 @@ function App() {
       </nav>
 
       <div className = "main-container">
-        <Object3D></Object3D>
-        <div className="main-container--message">
-          <p style = {{fontSize: 30, fontWeight: 600}}>서울시립대학교 컴퓨터 학술 동아리</p>
-          <p style = {{fontSize: 60, fontWeight: 700}}>Hello, QUIPU!</p>
+
+        {isSmallScreen ? ( <Object3D2 /> ) : ( <Object3D1 /> )}
+
+        <div className="main-container__message">
+          <p className="main-container__message--sentence1">서울시립대학교 <br></br>컴퓨터 학술 동아리</p>
+          <p className="main-container__message--sentence2">Hello, QUIPU!</p>
         </div>
       </div>
 
     </div>
   );
 }
-
-
 
 export default App;
