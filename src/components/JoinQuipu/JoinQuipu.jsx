dd5ff9340b23dc4bd2ec6f9d9fba96e61d3ce515
Junsub Cha <spd6041@naver.com>
2024-02-05 15:12:37 +0900

    chore : footer 완성

--- src/components/JoinQuipu/JoinQuipu.jsx
+++ src/components/JoinQuipu/JoinQuipu.jsx
@@ -140,7 +140,14 @@ function JoinQuipu() {
                 </div>
 
                 <div className="footer">
-                    
+                    <div className="footer-content">
+                        <p>카카오톡&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://open.kakao.com/o/sO45q17f"target="_blank">퀴푸문의사항</a></p>
+                        <p>인스타그램&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://www.instagram.com/uos_quipu?igsh=MTVjZTFhaXl6NGRoZQ=="target="_blank">@uos_quipu</a></p>
+                        <p>동아리방 위치&nbsp;&nbsp;|&nbsp;&nbsp;학생회관 342호</p>
+                        <br></br>
+                        <p>Copyrightⓒ2023.QUIPU. All rights reserved.</p>
+                        <p>study icon designed by Flaticon(Freepik)</p>
+                    </div>
                 </div>
             </div>
         </div>
