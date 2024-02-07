391b14b928944df90217b562e0c0cfd82c873507
YenaLey <yena.e121@gmail.com>
2024-02-07 19:17:35 +0900

    feat: about 디자인 완성

--- src/App.js
+++ src/App.js
@@ -58,9 +58,9 @@ function App() {
         {/* mobile에서 메뉴들 */}
         <div className={`navbar__menu--mobile ${menuOpen ? 'open' : ''}`}>
           <ul>
-            <li style={{ marginTop: '80px' }} onClick={closeMenu}><NavLink to="/home" onClick={closeMenu}>home</NavLink></li>
+            <li style={{ marginTop: '80px' }}><NavLink to="/home" onClick={closeMenu}>home</NavLink></li>
             <li><NavLink to="/about" onClick={closeMenu}>about</NavLink></li>
-            <li onClick={closeMenu}>
+            <li>
               <div className="activity-menu">
                 <NavLink to="/activity" onClick={closeMenu}>activity</NavLink>
                 <input id="activity-toggle" type="checkbox" onChange={toggleActivityDetail} />
@@ -70,13 +70,13 @@ function App() {
                 </label>
               </div>
               <div className={`activity-detail-menu ${isActivityDetailVisible ? 'visible' : ''}`}>
-                <li onClick={closeMenu}><NavLink to="/activity-detail#Study" onClick={closeMenu}>&nbsp;&nbsp;&nbsp;&nbsp;study</NavLink></li>
-                <li onClick={closeMenu}><NavLink to="/activity-detail#Friendship" onClick={closeMenu}>&nbsp;&nbsp;&nbsp;&nbsp;friendship</NavLink></li>
-                <li onClick={closeMenu}><NavLink to="/activity-detail#MT" onClick={closeMenu}>&nbsp;&nbsp;&nbsp;&nbsp;mt</NavLink></li>
+                <li><NavLink to="/activity-detail#Study" onClick={closeMenu}>&nbsp;&nbsp;&nbsp;&nbsp;study</NavLink></li>
+                <li><NavLink to="/activity-detail#Friendship" onClick={closeMenu}>&nbsp;&nbsp;&nbsp;&nbsp;friendship</NavLink></li>
+                <li><NavLink to="/activity-detail#MT" onClick={closeMenu}>&nbsp;&nbsp;&nbsp;&nbsp;mt</NavLink></li>
               </div>
             </li>
-            <li onClick={closeMenu}><NavLink to="/recommend-site" onClick={closeMenu}>recommend site</NavLink></li>
-            <li onClick={closeMenu}><NavLink to="/join-quipu" >join Quipu</NavLink></li>
+            <li><NavLink to="/recommend-site" onClick={closeMenu}>recommend site</NavLink></li>
+            <li><NavLink to="/join-quipu" onClick={closeMenu}>join Quipu</NavLink></li>
           </ul>
         </div>
 
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,22 @@ import React, { useRef, useEffect, useState, memo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import './About.css';
 
+function getRandomColor() {
+  let hue;
+  const colorType = Math.floor(Math.random() * 3);
+  switch (colorType) {
+    case 0:
+      return `hsl(0, 0%, ${90 + Math.random() * 10}%)`;
+    case 1:
+      hue = Math.random() < 0.5 ? Math.random() * 30 : 300 + Math.random() * 60;
+      break;
+    case 2:
+      hue = 210 + Math.random() * 30;
+      break;
+  }
+  return `hsl(${hue}, 100%, 50%)`;
+}
+
 const Confetti = memo(({ position, rotationSpeed }) => {
   const mesh = useRef();
 
@@ -17,7 +33,7 @@ const Confetti = memo(({ position, rotationSpeed }) => {
   return (
     <mesh ref={mesh} position={position}>
       <planeGeometry args={[0.1, 0.1, 1, 1]} />
-      <meshBasicMaterial color={`hsl(${Math.random() * 360}, 100%, 50%)`} />
+      <meshBasicMaterial color={getRandomColor()} />
     </mesh>
   );
 });
@@ -27,7 +43,7 @@ function About() {
 
   useEffect(() => {
     const tempConfettis = [];
-    for (let i = 0; i < 200; i++) {
+    for (let i = 0; i < 100; i++) {
       tempConfettis.push({
         position: [Math.random() * 5 - 2.5, Math.random() * 5, Math.random() * 5 - 2.5],
         rotationSpeed: { x: Math.random() * 0.02, y: Math.random() * 0.02 },
@@ -36,9 +52,19 @@ function About() {
     setConfettis(tempConfettis);
   }, []);
 
+  const [backgroundState, setBackgroundState] = useState('');
+
+  const toggleBackground = () => {
+    if (backgroundState === 'drain' || backgroundState === '') {
+      setBackgroundState('fill');
+    } else {
+      setBackgroundState('drain');
+    }
+  };
+
   return (
     <>
-      <div className="About-container">
+      <div className={`About-container ${backgroundState}`} onClick={toggleBackground}>
         <Canvas
           camera={{ fov: 45, aspect: window.innerWidth / window.innerHeight, near: 0.1, far: 1000 }}
         >
@@ -52,14 +78,21 @@ function About() {
         </Canvas>
 
         <div className="About-content">
+          <div className="w-90" style={{ margin: '0px', flexDirection: 'row' }}>
+            <p style={{ fontSize: '40px', fontWeight: '700', color: '#C0CEFF' }}>About</p>
+            <p style={{ color: 'black' }}>click!</p>
+          </div>
           <div className="w-90">
             <p>
               안녕하세요! 서울시립대학교 중앙 컴퓨터 학술 동아리 QUIPU입니다.
               <br></br>
-              'Quipu'는 문자 없이 끈과 매듭으로 정보를 기록하고 전달하는 고대 잉카 제국에서 사용된 시스템을 뜻합니다.
+              '퀴푸'는 문자 없이 끈과 매듭으로 정보를 기록하고 전달하는 고대 잉카 제국에서 사용된 시스템을 뜻합니다.
               <br></br>
-              이 어원에 영감을 받아 QUIPU는 다음 세 가지를 핵심 키워드로 삼고 있습니다.
+              이러한 어원에 따른 QUIPU의 핵심 키워드 세 가지를 소개합니다!
             </p>
+          </div>
+          <div className="w-90" style={{ margin: '0px' }}>
+            <p style={{ fontSize: '30px', fontWeight: '700', color: '#C0CEFF' }}>Our Keyword</p>
           </div>
           <div className="w-90">
             <div className="w-30">
@@ -67,14 +100,14 @@ function About() {
             </div>
             <div className="w-60">
               <p>
-                Quipu의 매듭은 각각 다른 정보와 지식을 담고 있듯이, 동아리 구성원 각자의 잠재력이 모여 더 큰 가치를 창출합니다.
+                퀴푸의 매듭은 각각 다른 정보와 지식을 담고 있듯이, QUIPU 구성원 각자의 잠재력이 모여 더 큰 가치를 창출합니다.
               </p>
             </div>
           </div>
-          <div className="w-90">
+          <div className="w-90 harmony">
             <div className="w-60">
               <p>
-                서로 다른 매듭과 실이 하나의 Quipu를 완성시키듯, 동아리는 개개인의 다양성을 조화롭게 엮어 공동의 목표를 향해 나아갑니다.
+                서로 다른 매듭과 실이 하나의 퀴푸를 완성시키듯, QUIPU는 개개인의 다양성을 조화롭게 엮어 공동의 목표를 향해 나아갑니다.
               </p>
             </div>
             <div className="w-30">
@@ -87,23 +120,26 @@ function About() {
             </div>
             <div className="w-60">
               <p>
-                Quipu에 사용된 다양한 색상과 길이의 실처럼, 동아리는 다양한 배경과 생각을 가진 사람들이 모여 새로운 아이디어와 혁신을 만들어냅니다.
+                퀴푸에 사용된 다양한 색상과 길이의 실처럼, QUIPU는 다양한 배경과 생각을 가진 사람들이 모여 새로운 아이디어와 혁신을 만들어냅니다.
               </p>
             </div>
           </div>
+          <div className="w-90" style={{ margin: '0px' }}>
+            <p style={{ fontSize: '30px', fontWeight: '700', color: '#C0CEFF' }}>Welcome!</p>
+          </div>
           <div className="w-90">
             <p>
-              QUIPU에서는 프로그래밍 언어 기초 공부, 개발, 공모전 등 컴퓨터와 관련하여 다양하게 스터디를 개설하거나 참여할 수 있습니다.
+              QUIPU에서는 프로그래밍 언어 기초 공부, 개발, 공모전 등 컴퓨터와 관련하여 다양하게 스터디를 개설하거나 스터디에 참여할 수 있습니다.
               <br></br>
-              창의적이고 협력적인 분위기로 개개인이 성장할 수 있는 환경을 조성하는 것을 목표로 하며, 교내에서 다양하게 컴퓨터에 관련한 활동 경험을 쌓을 수 있도록 노력하고 있습니다.
+              자유롭고 협력적인 분위기로 개개인이 성장할 수 있는 환경을 조성하는 것을 목표로 하며, 교내에서 컴퓨터와 관련하여 다양한 경험을 쌓을 수 있도록 노력하고 있습니다.
               <br></br>
               컴퓨터에 관심있는 서울시립대 학우분들 모두 환영합니다!
             </p>
           </div>
-          <div className="w-90">
-              <p>회장 차준섭&nbsp;|&nbsp;</p>
-              <p>부회장 하진혁&nbsp;|&nbsp;</p>
-              <p>총무 정민욱</p>
+          <div className="w-90 leaders" style={{ justifyContent: 'center' }}>
+            <p>회장 차준섭&nbsp;|&nbsp;</p>
+            <p>부회장 하진혁&nbsp;|&nbsp;</p>
+            <p>총무 정민욱</p>
           </div>
         </div>
       </div>
