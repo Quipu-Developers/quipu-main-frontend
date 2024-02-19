871b3c86108d08e5c8bed1800a465c149e019566
ej9374 <ej9374@naver.com>
2024-02-19 12:17:23 +0900

    chore: recommend-site 모바일버전 아이콘 위치 수정

--- src/components/RecommendSite/RecommendSite.jsx
+++ src/components/RecommendSite/RecommendSite.jsx
@@ -32,40 +32,45 @@ function Icon({ img, name, link, exp}) {
     );
 }
 
-
 function RecommendSite(){
-    const targetRef = useRef(null);
     
-    // useEffect(() => {    
-    //     const handleScroll = () => {
-    //         targetRef.current.style.position = window.scrollY > 0 ? "fixed" : "static";
-    //     };
-    //     window.addEventListener("scroll", handleScroll);
-    //     return () => {
-    //         window.removeEventListener("scroll", handleScroll);
-    //     };
-    // }, []);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return(
         <div>
             <div className="blank-navbar-pc"></div>
             <div className="blank-navbar-mobile"></div>
             <div className="recommend-site">
                 <div className="recommend-site__title">Recommend Site</div>
-                <div className="recommend-site__box" ref={targetRef}>
-                    <div className="recommend-site__box--top">
-                        <Icon img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
-                        <Icon img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
-                        <Icon img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
-                        <Icon img="../RecommendSite-img/w3Schools.png" name="W3 Schools"  exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
-                    </div>
-                    <div className="recommend-site__box--bottom">
-                        <Icon img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
-                        <Icon img="../RecommendSite-img/replit.png" name="Replit" exp="코드 작성 및 실행 가능한 온라인 플랫폼" link="https://replit.com/" />
-                        <Icon img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
+                <div className="recommend-site__container">
+                    <div className="recommend-site__box">
+                        <div className="recommend-site__box--top">
+                            <Icon img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
+                            <Icon img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
+                            <Icon img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
+                            <Icon img="../RecommendSite-img/w3Schools.png" name="W3 Schools"  exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
+                        </div>
+                        <div className="recommend-site__box--bottom">
+                            <Icon img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
+                            <Icon img="../RecommendSite-img/replit.png" name="Replit" exp="코드 작성 및 실행 가능한 온라인 플랫폼" link="https://replit.com/" />
+                            <Icon img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
+                        </div>
                     </div>
                 </div>
                 <div className="recommend-site__container--behind">
-                    <div className="recommend-site__box--behind">
+                    <div className="recommend-site__box--behind" style={{ transform: `translateX(${scrollPosition}px)`}}>
                         <Icon img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
                         <Icon img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
                         <Icon img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
