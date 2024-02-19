3e6253fa72038c37682e796da64aa29a1591aaa9
YenaLey <yena.e121@gmail.com>
2024-02-19 23:37:04 +0900

    feat: recommend site 가로 스크롤 추가

--- src/components/RecommendSite/RecommendSite.jsx
+++ src/components/RecommendSite/RecommendSite.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 import './RecommendSite.css';
 
 // pc 아이콘
@@ -33,8 +32,11 @@ function Icon({ img, name, link, exp }) {
 //mobile 아이콘
 function Icon2({ img, name, link, exp, index, scrollPosition }) {
     const [isListHover, setIsListHover] = useState(false);
+
     const multiplier = index % 2 === 0 ? -0.1 : 0.1;
     const movement = scrollPosition * multiplier;
+
+    console.log(`translateX(${movement}px)`);
 
     return (
         <div
@@ -66,86 +68,97 @@ function RecommendSite() {
 
     const [scrollPosition, setScrollPosition] = useState(0);
 
-    const handleScroll = () => {
-        const position = window.scrollY;
-        setScrollPosition(position);
-        setScrollPosition(prevPosition => prevPosition + 1);
-        setScrollPosition(prevPosition => prevPosition - 1);
+    // const handleScroll = () => {
+    //     const newPosition = window.scrollY;
+    //     console.log(newPosition);
+    //     setScrollPosition(newPosition);
+    // };
+
+    const scrollContainerRef = useRef(null);
+
+    const handleWheel = (e) => {
+        if (scrollContainerRef.current) {
+            const { deltaY } = e;
+            scrollContainerRef.current.scrollLeft += deltaY;
+        }
     };
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true });
+        const container = scrollContainerRef.current;
+        if (container) {
+            container.addEventListener('wheel', handleWheel);
+        }
 
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            if (container) {
+                container.removeEventListener('wheel', handleWheel);
+            }
         };
     }, []);
 
     return (
-        <div>
+        <div ref={scrollContainerRef}  className="recommend-site">
             <div className="blank-navbar-pc"></div>
             <div className="blank-navbar-mobile"></div>
-            <div className="recommend-site">
-                <div className="recommend-site__title">Recommend Site</div>
-                <div className="recommend-site__container">
-                    <div className="recommend-site__box">
-                        <div className="recommend-site__box--top">
-                            <Icon img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
-                            <Icon img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
-                            <Icon img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
-                            <Icon img="../RecommendSite-img/w3Schools.png" name="W3 Schools" exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
-                        </div>
-                        <div className="recommend-site__box--bottom">
-                            <Icon img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
-                            <Icon img="../RecommendSite-img/replit.png" name="Replit" exp="코드 작성 및 실행 가능한 온라인 플랫폼" link="https://replit.com/" />
-                            <Icon img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
-                        </div>
+            <div className="recommend-site__title">Recommend Site</div>
+            <div className="recommend-site__container">
+                <div className="recommend-site__box">
+                    <div className="recommend-site__box--top">
+                        <Icon img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
+                        <Icon img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
+                        <Icon img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
+                        <Icon img="../RecommendSite-img/w3Schools.png" name="W3 Schools" exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
+                    </div>
+                    <div ref={scrollContainerRef} className="recommend-site__box--bottom">
+                        <Icon img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
+                        <Icon img="../RecommendSite-img/replit.png" name="Replit" exp="코드 작성 및 실행 가능한 온라인 플랫폼" link="https://replit.com/" />
+                        <Icon img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
                     </div>
                 </div>
-                <div className="recommend-site__container--behind">
-                    <div className="recommend-site__box--behind">
-                        <Icon2 
+            </div>
+            <div className="recommend-site__container--behind">
+                <div className="recommend-site__box--behind">
+                    <Icon2
                         key={1}
                         index={1}
                         scrollPosition={scrollPosition}
                         img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
-                        
-                        <Icon2 
+
+                    <Icon2
                         key={2}
                         index={2}
                         scrollPosition={scrollPosition}
                         img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
 
-                        <Icon2 
+                    <Icon2
                         key={3}
                         index={3}
                         scrollPosition={scrollPosition}
                         img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
 
-                        <Icon2 
+                    <Icon2
                         key={4}
                         index={4}
                         scrollPosition={scrollPosition}
                         img="../RecommendSite-img/w3Schools.png" name="W3 Schools" exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
 
-                        <Icon2 
+                    <Icon2
                         key={5}
                         index={5}
                         scrollPosition={scrollPosition}
                         img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
 
-                        <Icon2 
+                    <Icon2
                         key={6}
                         index={6}
                         scrollPosition={scrollPosition}
                         img="../RecommendSite-img/replit.png" name="Replit" link="https://replit.com/" />
 
-                        <Icon2 
+                    <Icon2
                         key={7}
                         index={7}
                         scrollPosition={scrollPosition}
                         img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
-                    </div>
                 </div>
             </div>
         </div>
