f830a8ae7f68337f3296284f82dc5e5c5c8cba10
YenaLey <yena.e121@gmail.com>
2024-02-20 04:06:36 +0900

    feat: recommend site 디자인 수정

--- src/components/RecommendSite/RecommendSite.jsx
+++ src/components/RecommendSite/RecommendSite.jsx
@@ -24,7 +24,7 @@ function Icon({ img, name, link, exp }) {
                 <img width="60px" height="60px" src={img || ''} alt={name}></img>
                 <div>
                     <p style={{ fontSize: "15px", fontWeight: 'bold', margin: "5px" }}>{name}</p>
-                    {isListHover ? <p style={{fontSize: "12px", margin: "5px"}}>{exp}</p> : null}
+                    {isListHover ? <p style={{ fontSize: "12px", margin: "5px" }}>{exp}</p> : null}
                 </div>
             </div>
         </div>
@@ -49,11 +49,14 @@ function Icon2({ img, name, link, exp }) {
             <div className="recommend-site__icon--top2">
             </div>
             <div className="recommend-site__icon--body">
-                <img width="60px" height="60px" src={img || ''} alt={name}></img>
-                <p onClick={(e) => {
-                    window.open(link, '_blank');
-                }}>{name}</p>
-                {isListHover ? <p className="hover-text">what?<br></br>{exp}</p> : null}
+                <img className="recommend-site__icon--body-img" src={img || ''} alt={name}></img>
+                <div>
+                    <p style={{ fontSize: "15px", fontWeight: 'bold', margin: "5px" }}
+                        onClick={(e) => {
+                            window.open(link, '_blank');
+                        }}>{name}</p>
+                    {isListHover ? <p className="recommend-site__icon--body-exp">{exp}</p> : null}
+                </div>
             </div>
         </div>
     );
@@ -84,50 +87,54 @@ function RecommendSite() {
     }, []);
 
     return (
-        <div ref={scrollContainerRef}  className="recommend-site">
-            <div className="blank-navbar-pc"></div>
+        <>
             <div className="blank-navbar-mobile"></div>
-            <div className="recommend-site__title">Recommend Site</div>
-            <div className="recommend-site__container">
-                <div className="recommend-site__box">
-                    <div className="recommend-site__box--top">
-                        <Icon img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
-                        <Icon img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
-                        <Icon img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
-                        <Icon img="../RecommendSite-img/w3Schools.png" name="W3 Schools" exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
+            <div ref={scrollContainerRef} className="recommend-site">
+                <div className="blank-navbar-pc"></div>
+                <div className="recommend-site__title">Recommend Site</div>
+                <div className="recommend-site__container">
+                    <div className="recommend-site__box">
+                        <div className="recommend-site__box--top">
+                            <Icon img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
+                            <Icon img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
+                            <Icon img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
+                            <Icon img="../RecommendSite-img/w3Schools.png" name="W3 Schools" exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
+                            <div className="recommend-site__icon--blank"></div>
+                        </div>
+                        <div ref={scrollContainerRef} className="recommend-site__box--bottom">
+                            <Icon img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
+                            <Icon img="../RecommendSite-img/replit.png" name="Replit" exp="코드 작성 및 실행 가능한 온라인 플랫폼" link="https://replit.com/" />
+                            <Icon img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
+                        </div>
                     </div>
-                    <div ref={scrollContainerRef} className="recommend-site__box--bottom">
-                        <Icon img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
-                        <Icon img="../RecommendSite-img/replit.png" name="Replit" exp="코드 작성 및 실행 가능한 온라인 플랫폼" link="https://replit.com/" />
-                        <Icon img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
+                </div>
+                <div className="recommend-site__container--behind">
+                    <div className="recommend-site__box--behind">
+                        <Icon2
+                            img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
+
+                        <Icon2
+                            img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
+
+                        <Icon2
+                            img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
+
+                        <Icon2
+                            img="../RecommendSite-img/w3Schools.png" name="W3 Schools" exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
+
+                        <Icon2
+                            img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
+
+                        <Icon2
+                            img="../RecommendSite-img/replit.png" name="Replit" exp="코드 작성 및 실행 가능한 온라인 플랫폼" link="https://replit.com/" />
+
+                        <Icon2
+                            img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
+                        <div className="recommend-site__icon--blank"></div>
                     </div>
                 </div>
             </div>
-            <div className="recommend-site__container--behind">
-                <div className="recommend-site__box--behind">
-                    <Icon2
-                        img="../RecommendSite-img/chatGPT.png" name="ChatGPT" exp="AI 챗봇, 문서 작성 및 정보 검색 지원" link="https://chat.openai.com/" />
-
-                    <Icon2
-                        img="../RecommendSite-img/leetCode.png" name="LeetCode" exp="코딩 인터뷰 특화, 기술 기업 스타일 문제 사이트" link="https://leetcode.com/" />
-
-                    <Icon2
-                        img="../RecommendSite-img/hackerRank.png" name="HackerRank" exp="코딩 테스트 중심의 실무 기술 평가 사이트" link="https://www.hackerrank.com/" />
-
-                    <Icon2
-                        img="../RecommendSite-img/w3Schools.png" name="W3 Schools" exp="웹 개발 학습 자료 제공 사이트" link="https://www.w3schools.com/" />
-
-                    <Icon2
-                        img="../RecommendSite-img/baekJoon.png" name="BaekJoon" exp="알고리즘 문제 풀이 경쟁 사이트" link="https://www.acmicpc.net/" />
-
-                    <Icon2
-                        img="../RecommendSite-img/replit.png" name="Replit" link="https://replit.com/" />
-
-                    <Icon2
-                        img="../RecommendSite-img/computerWorld.png" name="Computer World" exp="IT 최신 뉴스 및 인사이트 제공 사이트" link="https://www.computerworld.com/" />
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
