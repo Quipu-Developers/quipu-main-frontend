6077f9f887b9c3fe33f5b8a54c265bc095455285
YenaLey <yena.e121@gmail.com>
2024-02-25 00:59:29 +0900

    chore: 에러페이지 수정

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -12,7 +12,7 @@ function Error() {
                 <h2>Please Try Again</h2>
                 <p>예상치 못한 오류가 발생했습니다.</p>
                 <p>나중에 다시 시도하거나,</p>
-                <p>문제가 지속될 경우 <span><a href="https://www.instagram.com/uos_quipu?igsh=MTVjZTFhaXl6NGRoZQ==" target="_blank">@uos_quipu</a></span>&nbsp;에 문의해 주세요</p>
+                <p>문제가 지속될 경우 <span><a href="https://open.kakao.com/o/sO45q17f" target="_blank">퀴푸 지원팀</a></span>&nbsp;에 문의해 주세요</p>
                 <button onClick={goHome}>GO TO HOME</button>
             </div>
         </div>
