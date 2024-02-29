b3f2f4c6963044e0049ba19590b32251e9de6e9e
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2024-03-01 06:09:53 +0900

    Update JoinQuipu.jsx

--- src/components/JoinQuipu/JoinQuipu.jsx
+++ src/components/JoinQuipu/JoinQuipu.jsx
@@ -77,7 +77,7 @@ function JoinQuipu() {
             textAreaContent: textAreaContent
         };
 
-        axios.post('http://3.39.180.232:2395/api/data', formData, {
+        axios.post('https://quipu-main-server.site/api/data', formData, {
             headers: {
                 'Content-Type': 'application/json',
             },
