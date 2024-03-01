568730561925d0c94db83cb77319fcb3839ae0c0
YenaLey <yena.e121@gmail.com>
2024-03-01 19:16:38 +0900

    feat: 환경변수 삭제

--- src/components/JoinQuipu/JoinQuipu.jsx
+++ src/components/JoinQuipu/JoinQuipu.jsx
@@ -6,7 +6,7 @@ import Error from '../Error/Error';
 
 function JoinQuipu() {
     
-    const isRecruiting = true; //모집 기간 여부
+    const isRecruiting = false; //모집 기간 여부
     const location = useLocation();
 
     const [entryType, setEntryType] = useState('newEntry');
@@ -77,7 +77,7 @@ function JoinQuipu() {
             textAreaContent: textAreaContent
         };
 
-        axios.post(`${process.env.REACT_APP_API_URL}/data`, formData, {
+        axios.post('https://quipu-main-server.site/api/data', formData, {
             headers: {
                 'Content-Type': 'application/json',
                 'Origin': 'https://uos-quipu.vercel.app'
