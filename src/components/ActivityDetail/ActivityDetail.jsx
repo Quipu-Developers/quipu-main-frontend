128e7fa225db9874847ac3ac089732e78fa696a4
YenaLey <yena.e121@gmail.com>
2024-03-01 20:55:33 +0900

    chore: activity 2024 디폴트 수정

--- src/components/ActivityDetail/ActivityDetail.jsx
+++ src/components/ActivityDetail/ActivityDetail.jsx
@@ -29,7 +29,7 @@ function ActivityDetail() {
         setActiveTab(tabName);
     };
 
-    const [selectedYear, setSelectedYear] = useState('2023');
+    const [selectedYear, setSelectedYear] = useState('2024');
 
     const navigate = useNavigate();
 
