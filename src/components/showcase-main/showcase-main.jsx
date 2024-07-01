206be120da6c03974927b4b2832ccb808e835671
juhyun <wngus5861@naver.com>
2024-07-01 19:49:19 +0900

    init commit

---
+++ src/components/showcase-main/showcase-main.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import './JoinQuipu.css';
+import Error from '../Error/Error';
