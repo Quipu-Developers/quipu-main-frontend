3f77d25d9862b562e6cbe52269019f3b60519cbf
lhimjy <lhimjy1105@naver.com>
2024-07-18 13:52:04 +0900

    chore : FAQ 및 폰트 색상, error code  등 변경

--- src/components/JoinQuipu/Dropdown.jsx
+++ src/components/JoinQuipu/Dropdown.jsx
@@ -4,8 +4,8 @@ import { NavHashLink as NavLink } from 'react-router-hash-link';
 function Dropdown(props){
   return (
     <ul className="dropdown-menu">
-      <li><NavLink to="/#join-quipu" onClick={() => props.setSelectedPage('general')}>General</NavLink></li>
-      <li><NavLink to="/#join-quipu" onClick={() => props.setSelectedPage('development')}>Development</NavLink></li>
+      <li><NavLink to="/#join-quipu" onClick={() => props.setSelectedPage('general')}>일반 부원</NavLink></li>
+      <li><NavLink to="/#join-quipu" onClick={() => props.setSelectedPage('development')}>개발 부원</NavLink></li>
     </ul>
   );
 }
