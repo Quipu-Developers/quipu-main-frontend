15750f27b77d2b9a8c8b90bb35844970ed9d0150
yezzero <yeyeonggim06@gmail.com>
2024-07-24 04:16:59 +0900

    Style: Interview 페이지 레이아웃 구현

--- src/components/Interview/Interview.jsx
+++ src/components/Interview/Interview.jsx
@@ -1,61 +1,96 @@
-import { NavHashLink as NavLink } from 'react-router-hash-link';
+import { useState, useEffect } from 'react';
 import './Interview.css';
-import ReactPaginate from "react-paginate";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-import React, { useState, useEffect, useRef } from 'react';
+import { profile } from './interview_data';
+import * as SolarIconSet from "solar-icon-set";
 
+function Interview() {
+  const [index, setIndex] = useState(0);
+  // const [windowWidth, setWindowWidth] = useState(window.outerWidth);
 
-const Pagination = ({ pageCount, onPageChange, currentPage }) => {
+  // useEffect(() => {
+  //   const handleResize = () => setWindowWidth(window.outerWidth);
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
+
   return (
-    <ReactPaginate
-      previousLabel={<FiChevronLeft />}
-      nextLabel={<FiChevronRight />}
-      pageCount={pageCount}
-      onPageChange={onPageChange}
-      containerClassName={"pagination"}
-      pageLinkClassName={"pagination__link"}
-      activeLinkClassName={"pagination__link__active"}
-    />
-  );
-};
-function Interview({ pageCount, onPageChange, currentPage }) {
-
-  const handlePageChange = ({ selected }) => {
-    onPageChange(selected + 1);
-  };
-  return (
-    <div className='interviewmain-person'>
-      <div className='interviewmain-nav'>
-        <section className="interviewmain-logo"><NavLink to="/#home" smooth>QUIPU-DEV</NavLink></section>
+    <div className="interview-container">
+      <div className="interview-navbar">
+        <h4>QUIPU-DEV</h4>
+        <p>Interview</p>
       </div>
-      <div className='interviewmain-body'>
-        <h3 className='interviewmain-body-title'>Interview</h3>
-        <div className='interviewmain-container'>
-          <div className='interviewmain-pagenation'>
-
-            {pageCount > 0 && (
-              <Pagination
-                pageCount={Math.max(1, pageCount - 1)}
-                onPageChange={handlePageChange}
-                currentPage={currentPage}
-              />
-            )}
+      <div className="interview-index-container">
+        <div className="interview-index">
+          <div className="interview-left">
+            {(index != 0) && <p>{index}기</p>}
           </div>
-          <div className='profilebox'>
-            <div className='profile'></div>
-            <div className='profile'></div>
-            <div className='profile'></div>
-            <div className='profile'></div>
-            <div className='profile'></div>
-            <div className='profile'></div>
-            <div className='profile'></div>
-            <div className='profile'></div>
-            <div className='profile'></div>
+          <div className="interview-arrow">
+            {
+              (index != 0) &&
+              <p onClick={(e)=> {
+                if (index>0) {
+                  setIndex(index-1);
+                }
+                e.stopPropagation();
+              }}>&lt;</p>
+            }
+          </div>
+          <div className="interview-middle">
+            <h4>{index+1}기</h4>
+          </div>
+          <div className="interview-arrow">
+            {
+              (index != 2) &&
+              <p onClick={(e)=> {
+                if (index<3) {
+                  setIndex(index+1);
+                }
+                e.stopPropagation();
+              }}>&gt;</p>
+            }
+          </div>
+          <div className="interview-right">
+            {(index != 2) && <p>{index + 2}기</p>}
           </div>
         </div>
       </div>
+
+      <div className="interview-profile">
+        {
+          profile[index].map(function (element) {
+            return (
+              <div className="interview-profilebox">
+                <div className="interview-profile-top">
+                  <SolarIconSet.MenuDots color="#233EC8" size={32} iconStyle="Broken" />
+                </div>
+                <div className="interview-profile-content">
+                  <div className="interview-profile-img">
+                    <img src={process.env.PUBLIC_URL + element.imgUrl} />
+                  </div>
+                  <div className="interview-profile-dc">
+                    <h4>{element.name}</h4>
+                    <p>{element.readme}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+
+      <div className="interview-pagination">
+        <img src={process.env.PUBLIC_URL + '/Interview-img/arrow.png'} />
+      </div>
+
+      <div className="interview-footer">
+        <p>QUIPU-DEV</p>
+      </div>
+
+
     </div>
-  );
+  )
 }
+
+
 
 export default Interview;
---
+++ src/components/Interview/interview_data.jsx
@@ -0,0 +1,148 @@
+export const profile = [
+    [
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/Interview-img/박지후.png",
+            name: "박지후",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+    ],
+    [
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/곽철이.png",
+            name: "곽철이",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        }
+    ],
+    [
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        },
+        {
+            imgUrl: "/ShowcaseDetail-img/회의5.jpg",
+            name: "선배마라탕",
+            readme: "때 내용이 요소나 폰트, 차지하는 로렘 표준 때"
+        }
+    ]
+]
