b72644bde9b3b8fdbeaf344c324deb29ef236d00
YenaLey <yena.e121@gmail.com>
2024-07-28 17:44:05 +0900

    showcase eslint 수정

--- src/page/ShowcaseMain/showcaseMainMobile.jsx
+++ src/page/ShowcaseMain/showcaseMainMobile.jsx
@@ -1,36 +1,36 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './ShowcasemainMobile.css';
-import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 function Dropdown() {
-
-    return (
-        <>
-            <li className='Dropdown-list'>LIST1</li>
-            <li className='Dropdown-list'>LIST2</li>
-        </>
-    );
+  return (
+    <>
+      <li className="Dropdown-list">LIST1</li>
+      <li className="Dropdown-list">LIST2</li>
+    </>
+  );
 }
 
 function ShowcasemainMobile() {
-
-    const [view, setView] = useState(false);
-    return (
-        <div className='showcasemainMobile'>
-            <div className='showcasemainMobile-header'>
-                <div className='ShowcasemainMobile-arrow'>⬅️</div>
-                <ul className="ShowcasemainMobile-Dropdown" onClick={() => { setView(!view) }}>
-                    Drop down{" "}
-                    {view ? '⌃' : '⌄'}
-                    {view && <Dropdown />}
-                </ul>
-            </div>
-            <div className='showcasemainMobile-main'>
-                <button className='showcasemainMobile-main-view'>view</button>
-                <div className='showcasemainMobile-main-card'></div>
-            </div>
-
-        </div>
-    );
+  const [view, setView] = useState(false);
+  return (
+    <div className="showcasemainMobile">
+      <div className="showcasemainMobile-header">
+        <div className="ShowcasemainMobile-arrow">⬅️</div>
+        <ul
+          className="ShowcasemainMobile-Dropdown"
+          onClick={() => {
+            setView(!view);
+          }}
+        >
+          Drop down {view ? '⌃' : '⌄'}
+          {view && <Dropdown />}
+        </ul>
+      </div>
+      <div className="showcasemainMobile-main">
+        <button className="showcasemainMobile-main-view">view</button>
+        <div className="showcasemainMobile-main-card"></div>
+      </div>
+    </div>
+  );
 }
 export default ShowcasemainMobile;
