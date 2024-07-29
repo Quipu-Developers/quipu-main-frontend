0a02fa8dda9fd5b88b962f95cb94231cdcd82cf6
juhyun <wngus5861@naver.com>
2024-07-29 19:54:35 +0900

    FEAT: interview index mapping 문제 해결

--- src/page/Interview/Interview.jsx
+++ src/page/Interview/Interview.jsx
@@ -31,25 +31,25 @@ function Interview() {
       <div className="interview-index-container">
         <div className="interview-index">
           <div className="interview-left">
-            {(index !== 0) && <p>{index}기</p>}
+            {(index != 0) && <p>{index}기</p>}
           </div>
           <div className="interview-arrow">
             {
-              (index !== 0) &&
-              <p onClick={(e) => {
-                if (index > 0) {
-                  setIndex(index - 1);
+             (index != 0) &&
+             <p onClick={(e)=> {
+               if (index>0) {
+                 setIndex(index-1);
                 }
                 e.stopPropagation();
               }}>&lt;</p>
             }
           </div>
           <div className="interview-middle">
-            <h4>{index + 1}기</h4>
+            <h4>{index+1}기</h4>
           </div>
           <div className="interview-arrow">
             {
-              (index !== 2) &&
+              (index != 2) &&
               <p onClick={(e) => {
                 if (index < 3) {
                   setIndex(index + 1);
@@ -59,32 +59,72 @@ function Interview() {
             }
           </div>
           <div className="interview-right">
-            {(index !== 2) && <p>{index + 2}기</p>}
+            {(index != 2) && <p>{index + 2}기</p>}
           </div>
         </div>
       </div>
       <div className="interview-profile">
-        {
-          interview_data[index].map(function (element) {
-            return (
-              <div className="interview-profilebox" key={element.name} onClick={() => openDetailModal(element)}>
-                <div className="interview-profile-top">
-                  <SolarIconSet.MenuDots color="#233EC8" size={32} iconStyle="Broken" />
-                </div>
-                <div className="interview-profile-content">
-                  <div className="interview-profile-img">
-                    <img src={process.env.PUBLIC_URL + `/Interview-img/${element.img}`} alt={element.name} />
-                  </div>
-                  <div className="interview-profile-dc">
-                    <h4>{element.name}</h4>
-                    <p>{element.title}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })
-        }
-      </div>
+  {
+    index === 0 && interview_data[0].map(function (element) {
+      return (
+        <div className="interview-profilebox" key={element.name} onClick={() => openDetailModal(element)}>
+          <div className="interview-profile-top">
+            <SolarIconSet.MenuDots color="#233EC8" size={32} iconStyle="Broken" />
+          </div>
+          <div className="interview-profile-content">
+            <div className="interview-profile-img">
+              <img src={process.env.PUBLIC_URL + `/Interview-img/${element.img}`} alt={element.name} />
+            </div>
+            <div className="interview-profile-dc">
+              <h4>{element.name}</h4>
+              <p>{element.title}</p>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  }
+  {
+    index === 1 && interview_data[1].map(function (element) {
+      return (
+        <div className="interview-profilebox" key={element.name} onClick={() => openDetailModal(element)}>
+          <div className="interview-profile-top">
+            <SolarIconSet.MenuDots color="#233EC8" size={32} iconStyle="Broken" />
+          </div>
+          <div className="interview-profile-content">
+            <div className="interview-profile-img">
+              <img src={process.env.PUBLIC_URL + `/Interview-img/${element.img}`} alt={element.name} />
+            </div>
+            <div className="interview-profile-dc">
+              <h4>{element.name}</h4>
+              <p>{element.title}</p>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  }
+  {
+    index === 2 && interview_data[2].map(function (element) {
+      return (
+        <div className="interview-profilebox" key={element.name} onClick={() => openDetailModal(element)}>
+          <div className="interview-profile-top">
+            <SolarIconSet.MenuDots color="#233EC8" size={32} iconStyle="Broken" />
+          </div>
+          <div className="interview-profile-content">
+            <div className="interview-profile-img">
+              <img src={process.env.PUBLIC_URL + `/Interview-img/${element.img}`} alt={element.name} />
+            </div>
+            <div className="interview-profile-dc">
+              <h4>{element.name}</h4>
+              <p>{element.title}</p>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  }
+</div>
       {IsdetailOpen && (
         <div className="modal-overlay">
           <div className="modal-content">
--- src/page/Interview/interviewdetail.jsx
+++ src/page/Interview/interviewdetail.jsx
@@ -5,15 +5,22 @@ function InterviewDetail({ profile, closeModal }) {
   return (
     <div className="modal">
       <div className="modal-content">
-        <h2>곽곽이</h2>
+        <h2>{profile.name}</h2>
         {profile && (
           <>
+          <div className="modal-header">
             <img src={process.env.PUBLIC_URL + `/Interview-img/${profile.img}`} alt={profile.name} />
-            <h3>{profile.name}</h3>
             <p>{profile.title}</p>
+            <button className="modal-close" onClick={closeModal}>CLOSE</button>
+            </div>
+            <h3>자기소개</h3>
+            <p>{profile.introduction}</p>
+            <h3>활동후기</h3>
+            <p>{profile.review}</p>
+            <h3>프로젝트 이력</h3>
+            <p>{profile.project.join(' ')}</p>  {/* join()을 통해서 한칸씩 띄어쓰기가 되게 렌더링 */}
           </>
         )}
-        <button onClick={closeModal}>닫기</button>
       </div>
     </div>
   );
