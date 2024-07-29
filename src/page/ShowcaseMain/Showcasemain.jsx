85a7f5abfe61c17a162fd94adcba2739e56d28d0
juhyun <wngus5861@naver.com>
2024-07-29 20:34:12 +0900

    FEAT: showcasemain 더미데이터 연결

--- src/page/ShowcaseMain/Showcasemain.jsx
+++ src/page/ShowcaseMain/Showcasemain.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Showcasemain.css';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
-import showcase_data from '../../data/showcase_data';
+import {showcase_data} from '../../data/showcase_data.jsx';
 import ShowcaseDetail from '../ShowcaseDetail/Showcasedetail';
+
 
 
 function Showcasemain() {
     const [Isshowcasedetail, setIsShowcasedetail] = useState(false);
+    const [index, setIndex] = useState(0);
 
     function handleShowcasedetail(){
         setIsShowcasedetail(true);
@@ -19,28 +21,36 @@ function Showcasemain() {
             </div>
             <div className='showcasemain'>
                 <div className='section1'>
-                    <div className='List' onClick={()=>{}}>LIST</div>
-                    <div className='List' onClick={()=>{}}>LIST</div>
-                    <div className='List' onClick={()=>{}}>LIST</div>
-                    <div className='List' onClick={()=>{}}>LIST</div>
-                    <div className='List' onClick={()=>{}}>LIST</div>
-                    <div className='List' onClick={()=>{}}>LIST</div>
+                    <div className='List' onClick={(e)=>{setIndex(0)}}>LIST</div>
+                    <div className='List' onClick={(e)=>{setIndex(1)}}>LIST</div>
+                    <div className='List' onClick={(e)=>{setIndex(2)}}>LIST</div>
+                    <div className='List' onClick={(e)=>{setIndex(3)}}>LIST</div>
+                    <div className='List' onClick={(e)=>{setIndex(4)}}>LIST</div>
+                    <div className='List' onClick={(e)=>{setIndex(5)}}>LIST</div>
                 </div>
                 <div className='section2'>
                     <div className='gallery'>
-                        <div className="gallery1"><img/></div>
+                        <div className="gallery1">
+                        <img src={showcase_data[index].main_img[0]} alt={`Image for ${showcase_data[index].project_name}`} />
+                        </div>
                     </div>
                     <div className='gallery'>
-                        <div className="gallery2">QUIPU-DEV</div>
+                        <div className="gallery2">{showcase_data[index].project_name}</div>
                     </div>
                     <div className='gallery'>
+                        <div className='gallery3'>
+                        <img src={showcase_data[index].main_img[1]} alt={`Image for ${showcase_data[index].project_name}`} />
+                        </div>
                         <div className='gallery3'></div>
-                        <div className='gallery3'></div>
-                        <div className='gallery3'></div>
-                        <div className='gallery3'></div>
+                        <div className='gallery3'>
+                        <img src={showcase_data[index].main_img[2]} alt={`Image for ${showcase_data[index].project_name}`} />
+                        </div>
+                        <div className='gallery3'>
+                        <img src={showcase_data[index].main_img[3]} alt={`Image for ${showcase_data[index].project_name}`} />
+                        </div>
                     </div>
                     <div className='gallery'>
-                        <div className='gallery4'></div>
+                        <div className='gallery4'><img src={showcase_data[index].main_img[4]} alt={`Image for ${showcase_data[index].project_name}`} /></div>
                     </div>
                 </div>
                 <NavLink to="/showcase-detail" className="view-button">VIEW</NavLink>
