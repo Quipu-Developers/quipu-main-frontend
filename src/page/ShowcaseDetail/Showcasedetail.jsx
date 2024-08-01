f2dddc3401a2deb47b0686119922d26042f76317
juhyun <wngus5861@naver.com>
2024-08-01 16:51:44 +0900

    FEAT: showcasemain css 너비 수정

--- src/page/ShowcaseDetail/Showcasedetail.jsx
+++ src/page/ShowcaseDetail/Showcasedetail.jsx
@@ -1,8 +1,12 @@
 import { useEffect } from 'react';
 import './Showcasedetail.css';
 import { showcase_data } from '../../data/showcase_data';
+import { useLocation } from 'react-router-dom'; // 'react-router' 대신 'react-router-dom' 사용
 
 export default function Showcasedetail() {
+  const location = useLocation();
+  const index = location.state.index ?? 0; // 중괄호 안에 index만 사용
+
   useEffect(() => {
     const handleResize = () => console.log(window.outerWidth);
     window.addEventListener('resize', handleResize);
@@ -12,7 +16,7 @@ export default function Showcasedetail() {
   return (
     <div className="showcasedetail-container">
       <div className="showcasedetail-header">
-        <h1>{showcase_data[0].project_name}</h1>
+        <h1>{showcase_data[index].project_name}</h1>
       </div>
     </div>
   );
--- src/page/ShowcaseMain/Showcasemain.jsx
+++ src/page/ShowcaseMain/Showcasemain.jsx
@@ -1,13 +1,12 @@
 import './Showcasemain.css';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
-import {showcase_data} from '../../data/showcase_data.jsx';
-import ShowcaseDetail from '../ShowcaseDetail/Showcasedetail';
+import { showcase_data } from '../../data/showcase_data.jsx';
 import { useState } from 'react';
 
 
 function Showcasemain() {
-    const [Isshowcasedetail, setIsShowcasedetail] = useState(false);
     const [index, setIndex] = useState(0);
+    //useparameter로 index값을 받아와서 index값을 state로 저장
 
     return (
         <div className='showcasemain-container'>
@@ -16,46 +15,58 @@ function Showcasemain() {
             </div>
             <div className='showcasemain'>
                 <div className='section1'>
-                    <div className='List' onClick={(e)=>{setIndex(0)}}>LIST</div>
-                    <div className='List' onClick={(e)=>{setIndex(1)}}>LIST</div>
-                    <div className='List' onClick={(e)=>{setIndex(2)}}>LIST</div>
-                    <div className='List' onClick={(e)=>{setIndex(3)}}>LIST</div>
-                    <div className='List' onClick={(e)=>{setIndex(4)}}>LIST</div>
-                    <div className='List' onClick={(e)=>{setIndex(5)}}>LIST</div>
+                    <div className='List' onClick={(e) => { setIndex(0) }}>LIST</div>
+                    <div className='List' onClick={(e) => { setIndex(1) }}>LIST</div>
+                    <div className='List' onClick={(e) => { setIndex(2) }}>LIST</div>
+                    <div className='List' onClick={(e) => { setIndex(3) }}>LIST</div>
+                    <div className='List' onClick={(e) => { setIndex(4) }}>LIST</div>
+                    <div className='List' onClick={(e) => { setIndex(5) }}>LIST</div>
                 </div>
                 <div className='section2'>
                     <div className='gallery'>
                         <div className="gallery1">
-                        <img src={showcase_data[index].main_img[0]} alt={`Image for ${showcase_data[index].project_name}`} />
+                            <img src={showcase_data[index].main_img[0]} alt={`Image for ${showcase_data[index].project_name}`} />
                         </div>
                     </div>
                     <div className='gallery'>
-                        <div className="gallery2">{showcase_data[index].project_name}</div>
+                        <div className="gallery2">
+                            <h3>{showcase_data[index].project_name}</h3>
+                            </div>
                     </div>
                     <div className='gallery'>
                         <div className='gallery3'>
-                        <img src={showcase_data[index].main_img[1]} alt={`Image for ${showcase_data[index].project_name}`} />
+                            <img src={showcase_data[index].main_img[1]} alt={`Image for ${showcase_data[index].project_name}`} />
                         </div>
                         <div className='gallery3'>
+                            <div gallery3-text>
                             <h4>{showcase_data[index].goal}</h4>
-                            <p className='gallery-3-text'>{showcase_data[index].this_project}</p>
+                            <p>{showcase_data[index].this_project}</p>
+                            </div>
                         </div>
                         <div className='gallery3'>
-                        <img src={showcase_data[index].main_img[2]} alt={`Image for ${showcase_data[index].project_name}`} />
+                            <img src={showcase_data[index].main_img[2]} alt={`Image for ${showcase_data[index].project_name}`} />
                         </div>
                         <div className='gallery3'>
-                        <img src={showcase_data[index].main_img[3]} alt={`Image for ${showcase_data[index].project_name}`} />
+                            <img src={showcase_data[index].main_img[3]} alt={`Image for ${showcase_data[index].project_name}`} />
                         </div>
                     </div>
                     <div className='gallery'>
                         <div className='gallery4'><img src={showcase_data[index].main_img[4]} alt={`Image for ${showcase_data[index].project_name}`} /></div>
                     </div>
                 </div>
-                <NavLink to="/showcase-detail" className="view-button">VIEW</NavLink>
+                <NavLink
+                    to={{
+                        pathname: "/showcase-detail",
+                        state:{index: {index}} // index 값을 state로 전달
+                    }}
+                    className="view-button"
+                >
+                    VIEW
+                </NavLink>
+
             </div>
-            {Isshowcasedetail && <ShowcaseDetail />} 
         </div>
-        
-  );
+
+    );
 }
 export default Showcasemain;
