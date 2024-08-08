5430c9c881cec67e09c77a9c1e90bbef9ee06a56
juhyun <wngus5861@naver.com>
2024-08-08 20:08:29 +0900

    FEAT: showcasemain-mobile section2 구현

--- src/page/ShowcaseMain/showcaseMainMobile.jsx
+++ src/page/ShowcaseMain/showcaseMainMobile.jsx
@@ -3,16 +3,17 @@ import { NavHashLink as NavLink } from 'react-router-hash-link';
 import { useNavigate } from 'react-router-dom';
 import { showcase_data } from '../../data/showcase_data.jsx';
 import './ShowcasemainMobile.css';
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 function Dropdown({ setIndex }) { // setIndex를 props로 받음
   return (
-    <>
+    <div className="Dropdown-list">
       {showcase_data.map((item, idx) => (
-        <li className="Dropdown-list" key={idx} onClick={() => { setIndex(idx) }}>
+        <li key={idx} onClick={() => { setIndex(idx) }}>
           {item.project_name} {/* item.project_name으로 변경 */}
-        </li>
+        </li> 
       ))}
-    </>
+    </div>
   );
 }
 
@@ -30,19 +31,17 @@ function ShowcasemainMobile() {
   return (
     <div className="showcasemainMobile">
       <div className='showcasemainMobile-nav'>
-      <section className="showcasemainMobile-logo"><NavLink to="/">QUIPU</NavLink></section>
+      <section className="showcasemainMobile-logo"><NavLink to="/">QUIPU-DEV</NavLink></section>
       </div>
       <div className="showcasemainMobile-header">
-        <div className="ShowcasemainMobile-arrow">
-          <img src='../public/ShowcaseMain-img/arrowleft.png' alt="Arrow"/>
-        </div>
-        <ul
+      <ul
           className="ShowcasemainMobile-Dropdown"
           onClick={() => {
             setView(!view);
           }}
         >
-          Drop down {view ? '⌃' : '⌄'}
+          <p>Drop down</p>
+          <p>{view ? '▲' : '▼'}</p> 
           {view && <Dropdown setIndex={setIndex}/>} {/* setIndex를 Dropdown에 전달 */}
         </ul>
       </div>
@@ -57,7 +56,22 @@ function ShowcasemainMobile() {
             <img src={showcase_data[index].main_img[0]} alt={`Image for ${showcase_data[index].project_name}`} />
           </div>
         </div>
-        <div className='showcasemainMobile-section2'></div>
+        <div className='showcasemainMobile-section2'>
+          <div className='section2-gallery1'>
+          <img src={showcase_data[index].main_img[1]} alt={`Image for ${showcase_data[index].project_name}`} />
+
+          </div>
+          <div className='section2-text'>
+          <h4>{showcase_data[index].goal}</h4>
+          <p>{showcase_data[index].this_project}</p>
+          </div>
+          <div className='section2-gallery2'>
+          <img src={showcase_data[index].main_img[2]} alt={`Image for ${showcase_data[index].project_name}`} />
+          </div>
+          <div className='section2-gallery3'>
+          <img src={showcase_data[index].main_img[3]} alt={`Image for ${showcase_data[index].project_name}`} />
+          </div>
+        </div>
         <div className='showcasemainMobile-section3'>
           <div className='showcasemainMobile-gallery3'>
             <img src={showcase_data[index].main_img[4]} alt={`Image for ${showcase_data[index].project_name}`} />
